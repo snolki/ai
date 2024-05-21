fee71dbd637444b9b3071d29b0864b3e342f57a0
snolki <arikeasalar@gmail.com>
2024-05-21 22:49:20 +0200

    removed camera from train file

--- main.py
+++ main.py
@@ -50,15 +50,3 @@ model.fit(
     epochs=10
 )
 model.save('a.weights.h5')
-while(True): 
-	
-	ret, frame = vid.read() 
-
-	cv2.imshow('frame', frame) 
-
-	if cv2.waitKey(1) & 0xFF == ord('q'): 
-		break
-
-vid.release() 
-
-cv2.destroyAllWindows() 
